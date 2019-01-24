1242aa4b8928d88f74af9a2a22a359c259032c04
JeffHiggy <46974337+JeffHiggy@users.noreply.github.com>
2019-01-24 12:50:17 -0500

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,23 @@
+## Install.sh
+# This will install the links to the bashprofile
+
+# Backup existing bash profile if exist
+
+if [ -f ~/.bashrc ] || [ -f ~/.bash_profile ] || [ -d ~/.dotfiles ]; then
+	mkdir ~/.bashrc ~/.oldbashprofile
+fi
+
+if [ -f ~/.bashrc ]; then
+	mv ~/.bashrc ~/.oldbashprofile/.bashrc
+fi
+if [ -f ~/.bash_profile ]; then
+	mv ~/.bash_profile ~/.oldbashprofile/.bash_profile
+fi
+if [ -d ~/.dotfiles ]; then
+	mv ~/.dotfiles ~/.oldbashprofile/.dotfiles
+fi
+
+# Create links
+ln -s ~/.bashprofile/.bashrc ~/.bashrc
+ln -s ~/.bashprofile/.bash_profile ~/.bash_profile
+ln -s ~/.bashprofile/.dotfiles ~/.dotfiles
